383db9f517df37bd8159219daa991c53fcb4a31f
Saddam Hussain <70686353+BinSyed@users.noreply.github.com>
2021-07-13 17:25:21 +0400

    Add files via upload

---
+++ GraphQL.WebApi/Startup.cs
@@ -0,0 +1,62 @@
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using GraphQL.WebApi.Data;
+using GraphQL.WebApi.GraphQL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GraphQL.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("DefaultConnection"),
+                   b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddTransient<IDependencyResolver>(x => new FuncDependencyResolver(x.GetRequiredService));
+            services.AddTransient<DemoSchema>();
+            services.AddGraphQL(o => o.ExposeExceptions = true)
+                    .AddGraphTypes(ServiceLifetime.Transient);
+            services.Configure<KestrelServerOptions>(options => options.AllowSynchronousIO = true);
+            services.Configure<IISServerOptions>(options => options.AllowSynchronousIO = true);
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseGraphQL<DemoSchema>();
+            app.UseGraphQLPlayground(options: new GraphQLPlaygroundOptions());
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
